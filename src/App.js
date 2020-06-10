a9b9a14840a0f3d8869163cffe5f112b13cfcbe2
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-10 19:13:53 +0500

    new func created in App.js and new file hello.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import hello from "./hello.js"
+const Hi=({name})=>Hello World +name
+function hi({name}){
+    return "Hello world"; +name
+}
 
 function App({name,age}) {
   return (
     <div>
       Hello world from Pakistan again {name} Age ={age-2}
+      <hello></hello>
     </div>
   );
 }
---
+++ src/hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function hello(){
+    return<p> This is para in hello tag</p>
+}
+export default hello;
