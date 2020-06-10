b052111d154fafc52d34480a54323a5c3de20ad0
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-10 19:36:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 import hello from "./hello.js";
-const Hi= ({name})=> "Hello World" +name;
-function Hi({name}){
-    return "Hello World" +name;
-}
 
 function App({name,age}) {
   return (
     <div>
       Hello world from Pakistan again {name} Age ={age-2}
+      <br/>
       <hello></hello>
     </div>
   );
