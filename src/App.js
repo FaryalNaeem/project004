2c156f4dffb439c5b2139b06bd472392d8ab7b08
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-10 10:57:27 +0500

    undone all changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return
-    <div>Hello world from Pakistan</div>
+  return (
+    <div>
+      Hello world from Pakistan
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <span>Hello world by me </span>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
-// utnregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
