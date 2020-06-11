f8bb91029283e114bf5e67560b57c74ba7fa88f0
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-11 09:26:18 +0500

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App name= "Faryal" age= {24}/>,
+  <div>><App name= "Faryal" age= {24}/></div>,
   document.getElementById('root')
 );
 
