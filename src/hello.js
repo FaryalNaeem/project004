9cfcdb78462378f6fc463b337dfbd9066359843b
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-11 10:28:11 +0500

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 function hello(Firstname) {
-return ( <p>This is para in hello tag name:{Firstname}</p>)
+return ( <p>This is para in hello tag name:{Firstname}</p>) 
 }
 export default hello;
