fe6c60c8471d705672d1f18a3cbc72f8df199a0d
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-11 15:12:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
+import Hello from './hello';
 
 function App({name, age}) {
   return <div>
       Hello world from Pakistan again {name} {age}
       <br/>
-      <hello Firstname = {name}></hello>
+      <Hello Firstname = {name}></Hello>
     </div>
 }
 export default App;
