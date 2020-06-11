c81321cca4fdff898951a4476bef316e245237ed
FaryalNaeem <52119357+FaryalNaeem@users.noreply.github.com>
2020-06-11 13:31:05 +0500

    some cahnge in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import hello from './hello';
 
-function App({name,age}) {
+function App({name, age}) {
   return <div>
       Hello world from Pakistan again {name} {age}
       <br/>
